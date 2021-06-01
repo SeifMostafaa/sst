d0c2c1208f81cacb17eaca241756a278ed9b355f
Mohamed Zakarya <zakarya.fekry@myfave.com>
2021-06-01 13:24:43 +0200

    Create majors index

---
+++ app/controllers/majors_controller.rb
@@ -0,0 +1,17 @@
+class MajorsController < ApplicationController
+  def index
+    @majors = Major.all
+  end
+
+  def show; end
+
+  def new; end
+
+  def create; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+end
