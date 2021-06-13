d99aeac11b6ec7fcd96362809649ebc0d3e8620b
Mohamed Zakarya <zakarya.fekry@myfave.com>
2021-06-13 20:23:00 +0200

    Add operating_hours to time_tables by_student api

--- app/controllers/api/v1/time_tables_controller.rb
+++ app/controllers/api/v1/time_tables_controller.rb
@@ -6,15 +6,21 @@ class Api::V1::TimeTablesController < ApplicationController
   returns code: 401, desc: 'Unauthorized'
   returns code: 200,
           desc: 'Student Time Table' do
-    property :id, Integer, desc: "Time Table's' ID"
-    property :period, Integer, desc: "Time Table's Period"
-    property :day, %w[monday tuesday wednesday thursday friday saturday sunday],
-             desc: "Time Table's Day"
-    property :start_time, Time, desc: "Time Table's Start Time"
-    property :start_time, Time, desc: "Time Table's End Time"
-    property :subject_name_ar, String, desc: "Subject's Arabic Name"
-    property :subject_name_en, String, desc: "Subject's English Name"
-    property :subject_class_number, :number, desc: "Subject's Class Number"
+    property :operating_hours, Hash, desc: 'School Opetating Hours object' do
+      property :start_time, Time, desc: "School's start time"
+      property :end_time, Time, desc: "School's end time"
+    end
+    property :time_tables, Hash, desc: 'Time Tables array of objects' do
+      property :id, Integer, desc: "Time Table's' ID"
+      property :period, Integer, desc: "Time Table's Period"
+      property :day, %w[monday tuesday wednesday thursday friday saturday sunday],
+               desc: "Time Table's Day"
+      property :start_time, Time, desc: "Time Table's Start Time"
+      property :start_time, Time, desc: "Time Table's End Time"
+      property :subject_name_ar, String, desc: "Subject's Arabic Name"
+      property :subject_name_en, String, desc: "Subject's English Name"
+      property :subject_class_number, :number, desc: "Subject's Class Number"
+    end
   end
   def by_student
     @time_tables = current_user.time_tables.order(day: :asc)
--- app/views/api/v1/time_tables/by_student.json.jbuilder
+++ app/views/api/v1/time_tables/by_student.json.jbuilder
@@ -1,2 +1,9 @@
-json.array! @time_tables, partial: 'api/v1/time_tables/time_table',
-                          as: :time_table
+json.operating_hours do
+  json.start_time '07:00'
+  json.end_time '15:00'
+end
+
+json.time_tables do
+  json.array! @time_tables, partial: 'api/v1/time_tables/time_table',
+                            as: :time_table
+end
