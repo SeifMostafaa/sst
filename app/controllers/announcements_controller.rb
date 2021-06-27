aa953fa5a027a1269994ac159cb5c43ab812d3b9
Mohamed Zakarya <zakarya.fekry@myfave.com>
2021-06-27 08:33:01 +0200

    Create announcement creation page

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -5,13 +5,39 @@ class AnnouncementsController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @announcement = Announcement.new
+  end
 
-  def create; end
+  def create
+    @announcement = Announcement.new(announcement_params)
+
+    respond_to do |format|
+      if @announcement.save
+        format.html { redirect_to announcements_path, notice: 'Announcement was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
 
   def edit; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def announcement_params
+    params.require(:announcement).permit(
+      :title_ar,
+      :title_en,
+      :description_ar,
+      :description_en,
+      :announcement_type,
+      :user_type,
+      :attachment
+    )
+  end
 end
