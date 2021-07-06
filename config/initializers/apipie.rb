37daf86f55f27c88c6cfde830f25acf43dff28c6
Mohamed Zakarya <zakarya.fekry@myfave.com>
2021-07-06 16:03:30 +0200

    Disable apipie validation

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,8 +1,9 @@
 Apipie.configure do |config|
-  config.app_name                = "Sst"
-  config.api_base_url            = ""
-  config.doc_base_url            = "/docs/api"
+  config.app_name                = 'Sst'
+  config.api_base_url            = ''
+  config.doc_base_url            = '/docs/api'
   config.translate = false
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  config.validate = false
 end
